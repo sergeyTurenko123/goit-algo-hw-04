bb5499ab5c4a7f30c904fd0b48ec4236cfe169a3
sergeyTurenko123 <sergej_petrovich_8484@ukr.net>
2024-02-08 22:37:13 +0300

    dz4

--- dz4.py
+++ dz4.py
@@ -1,10 +1,8 @@
-# Створення файлу
+# Завдання №1:
 
 with open("path.txt","w") as fh:
-    fh.write("Alex Korp,3000\nNikita Borisenko,2000\nSitarama Raju,1000")
-
-# Завдання №1:
-    
+    fh.write("Alex Korp,3000\nNikita Borisenko,2000\nSitarama Raju,1000\nSerg Turenko,2000")
+  
 def total_salary(path):
     try:
         with open("path.txt","r") as fh:
@@ -23,6 +21,28 @@ def total_salary(path):
 total, average = total_salary("path.txt")
 
 
+# Завдання №2
 
-        
-   
+
+with open("get_cats_info.txt","w") as fh:
+    fh.write("60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5\n")
+
+def get_cats_info(get_cats_info):
+    try:
+        with open('get_cats_info.txt', "r") as file:
+            lines = file.readlines()
+            lists = []
+        for line in lines:
+            list_line = line.split(",")
+            lists.append({"id": list_line[0], "name": list_line[1], "age": int(list_line[2].strip())})
+        return lists
+    except Exception:
+        print("The file does not exist")
+
+
+cats_info = get_cats_info("get_cats_info.txt")
+print(cats_info)
+
+
+
+
