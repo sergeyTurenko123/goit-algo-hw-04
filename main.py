c35833978fbf014da879321a77cc569235512f25
sergeyTurenko123 <sergej_petrovich_8484@ukr.net>
2024-02-13 00:47:34 +0300

    dz4

--- main.py
+++ main.py
@@ -1,11 +1,67 @@
-from comand import add_contact, change_contact, show_phone, show_all
-
 def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
+    name, *args = user_input.split()
+    name = name.strip().lower()
+    return name, *args
+
+def add_contact(args):
+    name, phone = args
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        lists.update({list_line[0]: list_line[1].strip()})
+    if name in lists:
+        print("Contact exists. Re-record?")
+    else:
+        lists[name] = phone
+        with open("name_phone.txt","w", encoding = "utf-8") as fh:
+            for key, value in lists.items():
+                fh.write(f'{key},{value}\n')
+    return "Contact added."
+
+def change_contact(args):
+    name, phone = args
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        lists.update({list_line[0]: list_line[1].strip()})
+    if name in lists:
+        lists[name] = phone
+        with open("name_phone.txt","w", encoding = "utf-8") as fh:
+            for key, value in lists.items():
+                fh.write(f'{key},{value}\n')
+    else:
+        print("Contact does not exist. Will you write it down?")
+    return "Contact updated."
+
+def show_phone(args):
+    name = args
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        lists.update({list_line[0]: list_line[1].strip()})
+    if name in lists:
+        print(f"{name},{lists[name]}")
+    else:
+        print("Contact does not exist. Will you write it down?")
+    return show_phone(args)
+
+def show_all():
+    with open('name_phone.txt', "r", encoding = "utf-8") as fh:
+        lines = fh.readlines()
+    lists = {}
+    for line in lines:
+        list_line = line.split(",")
+        lists.update({list_line[0]: list_line[1].strip()})
+    return lists
 
 def main():
+    contacts = {}
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
