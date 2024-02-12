1eb3b48deda2b0c096e0b6b4b305e3644c4848a1
sergeyTurenko123 <sergej_petrovich_8484@ukr.net>
2024-02-13 00:48:46 +0300

    dz4

--- dz4.py
+++ dz4.py
@@ -23,8 +23,6 @@ total, average = total_salary("path.txt")
 
  # Завдання №2
 
-
-
 with open("get_cats_info.txt","w") as fh:
     fh.write("60b90c1c13067a15887e1ae1,Tayson,3\n60b90c2413067a15887e1ae2,Vika,1\n60b90c2e13067a15887e1ae3,Barsik,2\n60b90c3b13067a15887e1ae4,Simon,12\n60b90c4613067a15887e1ae5,Tessi,5\n")
 
@@ -43,6 +41,9 @@ def get_cats_info(get_cats_info):
 
 cats_info = get_cats_info("get_cats_info.txt")
 print(cats_info)
+
+
+
 
 
 
